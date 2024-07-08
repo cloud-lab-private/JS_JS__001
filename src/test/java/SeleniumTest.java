df9218d2ab1b2974908b14726cba0f1d192d6876
Nanz <113892126+iruthaya-nancy@users.noreply.github.com>
2024-07-08 14:16:09 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -18,10 +18,8 @@ public class SeleniumTest {
 
     @Before
     public void setUp() {
-        // Set up ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
 
-        // Create a new ChromeDriver instance
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         driver = new ChromeDriver(options);
